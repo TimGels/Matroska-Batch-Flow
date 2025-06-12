994b0ba5d66a31f4433700be592c31bbd3ff942b
TimGels <TimGels@users.noreply.github.com>
2025-06-12 13:00:11 +0200

    Add dialog status management and refactor ErrorDialog usage
    - Introduced `DialogStatusMessage` record to track dialog status.
    - Updated `ErrorDialog` to send status notifications on open/close.
    - Modified `InputPage.xaml` to enable & disable  `ListView` filedrop ability based on dialog status message.
    - Removed `ErrorDialog` instance from `MainPage.xaml` as UNO Platforms Skia target has a bug that immediately opens the dialog when it is included in the XAML code. So creation of the ErrorDialog now happens in the code behind when it is needed.

--- MatroskaBatchFlow.Uno/MatroskaBatchFlow.Uno/Presentation/InputPage.xaml.cs
+++ MatroskaBatchFlow.Uno/MatroskaBatchFlow.Uno/Presentation/InputPage.xaml.cs
@@ -1,3 +1,6 @@
+using CommunityToolkit.Mvvm.Messaging;
+using MatroskaBatchFlow.Uno.Presentation.Dialogs;
+
 namespace MatroskaBatchFlow.Uno.Presentation;
 /// <summary>
 /// An empty page that can be used on its own or navigated to within a Frame.
@@ -7,5 +10,11 @@ public sealed partial class InputPage : Page
     public InputPage()
     {
         this.InitializeComponent();
+
+        WeakReferenceMessenger.Default.Register<DialogStatusMessage>(this, (r, dialogMessage) =>
+        {
+            // If the dialog is open, disable file drop on the ListView.
+            this.FileDropListView.AllowDrop = !dialogMessage.IsOpen;
+        });
     }
 }
